82e5a1e7e652c239ff53333d255da827459e5a39
kajabla <jskestrel@gmail.com>
2023-04-26 00:09:42 -0700

    Simplified get_drm() to use filter_trt_tgt. Added get_EC_logM function to use drm::ED() to get an EC50.

--- R/drm.R
+++ R/drm.R
@@ -1,46 +1,66 @@
-#' Get a dose-response model object for the activity of a treatment on a target
+#' Get a drc dose-response model for the activity of a treatment on a target
 #'
 #' `get_drm()` runs [drc::drm()] to generate a 4-parameter logistic
-#' dose-response model for the effect of a given treatment on a target. It takes
-#' a dataframe that can include data for more than just the desired combination
-#' of treatment and target. The dataframe is filtered to contain only data for
-#' the desired combination before the model is fit.
+#' dose-response model for the effect of a given treatment on a target.
 #'
 #' @param data A dataframe containing the following columns:
-#' * "treatment" (default) or other treatment column name (see `trt_colname`)
-#' * "target" (default) or other target column name (see `tgt_colname`)
 #' * "activity" (default) or other activity column name (see `activity_colname`)
 #' * conc_logM: concentration of treatment in log(molar) units
-#' @param trt Name of the treatment to use in the model
-#' @param tgt Name of the target to use in the model
-#' @param trt_colname Name of the column containing treatments.
-#'     Default is "treatment". Typical alternatives might include e.g. "drug",
-#'     "additive", "supplement".
-#' @param tgt_colname Name of the column containing targets.
-#'     Default is "target". Typical alternatives might include e.g. "mutant",
-#'     "cell_line", "species".
 #' @param activity_colname Name of the column containing activity of treatment.
 #'     Default is "activity". Typical alternatives might include e.g.
 #'     "percent_inhibition", "viability", "growth".
-#'
+#' @param ... Additional parameters to pass on to [drc::drm()]
 #' @return A dose-response model object of class `drc`.
 #' @export
 #'
-get_drm <- function(data, trt, tgt,
-                    trt_colname="treatment",
-                    tgt_colname="target",
-                    activity_colname="activity"
-                    ){
-  assertthat::assert_that(assertthat::has_name(data, trt_colname),
-              msg = glue::glue("column {trt_colname} not found in data"))
-  assertthat::assert_that(assertthat::has_name(data, tgt_colname),
-              msg = glue::glue("column {tgt_colname} not found in data"))
+get_drm <- function(data, activity_colname="activity", ...){
   assertthat::assert_that(assertthat::has_name(data, activity_colname),
-              msg = glue::glue("column {activity_colname} not found in data"))
-  data_subset <- data |>
-    # unclear why you have to wrap embrace in get() but apparently you do
-    dplyr::filter(get({{ trt_colname }}) == trt,
-                  get({{ tgt_colname }}) == tgt)
+                          msg = glue::glue("column {activity_colname} not found in data"))
   # 4-param logistic model on pre-log-transformed data
-  return(drc::drm(get({{ activity_colname }})~conc_logM, data = data_subset, fct = drc::L.4()))
+  return(drc::drm(get({{ activity_colname }})~conc_logM,
+                  data = data, fct = drc::L.4()))
+}
+
+#' Title Get an EC value with drc for the activity of a treatment on a target
+#'
+#' `get_EC_logM()` runs [filter_trt_tgt()] and [get_drm()] to get a
+#' dose-response model of the effect of conc_logM on activity for the specified
+#' treatment and target, then extracts the effective concentration value of the
+#' model at the specified threshold using [drc::ED()].
+#'
+#' @param response_level The response level in percent for which to estimate the
+#'   effective concentration. This parameter is used as the `respLev` parameter
+#'   of [drc::ED()]. Should be between 0 and 100.
+#'
+#'   Be careful: this parameter can be confusing when the response to treatment
+#'   is a decrease in activity. This is because the response level represents
+#'   the percent of the way from whichever asymptote is a lower activity level
+#'   to whichever is higher rather than from the zero-dose asymptote to the
+#'   infinite-dose asymptote. For example, if activity starts at 100 at 0 dose
+#'   and goes down to 0 at infinite dose, then `get_ED_logM(response_level=75)`
+#'   will return the concentration that gives an activity of 75, even though you
+#'   might naively expect that it would return the concentration that gives an
+#'   activity of 25 (75% of the way to the infinite-dose effect). This is
+#'   debatably a bug in [drc::ED()].
+#'
+#'   Be careful: this parameter can also be confusing for data not normalized to
+#'   100. For example, if activity e.g. starts at 200 at 0 dose and increases to
+#'   500 at infinite dose, then `get_ED_logM(response_level=50)` will return the
+#'   concentration that gives an activity of 350 (50% of the way from 200 to
+#'   500).
+#'
+#' @inheritParams filter_trt_tgt
+#' @inheritParams get_drm
+#'
+#' @return The effective concentration value at the desired response level in
+#'   log10(molar) units.
+#' @export
+#'
+get_EC_logM <- function(data, trt, tgt, response_level=50,
+                   trt_colname="treatment",
+                   tgt_colname="target",
+                   activity_colname="activity"){
+  data <- filter_trt_tgt(data, trt, tgt, trt_colname, tgt_colname)
+  drm <- get_drm(data, activity_colname=activity_colname)
+return(drc::ED(drm, response_level, display=FALSE)[1,1])
 }
