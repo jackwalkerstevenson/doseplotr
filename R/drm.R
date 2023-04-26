b1fa72443b498203ec951aa37f351a8dd6d00352
kajabla <jskestrel@gmail.com>
2023-04-26 00:39:31 -0700

    Changed "colname" to "col" and slightly shortened error messages in drm

--- R/drm.R
+++ R/drm.R
@@ -4,20 +4,20 @@
 #' dose-response model for the effect of a given treatment on a target.
 #'
 #' @param data A dataframe containing the following columns:
-#' * "activity" (default) or other activity column name (see `activity_colname`)
+#' * "activity" (default) or other activity column name (see `activity_col`)
 #' * conc_logM: concentration of treatment in log(molar) units
-#' @param activity_colname Name of the column containing activity of treatment.
+#' @param activity_col Name of the column containing activity of treatment.
 #'     Default is "activity". Typical alternatives might include e.g.
 #'     "percent_inhibition", "viability", "growth".
 #' @param ... Additional parameters to pass on to [drc::drm()]
 #' @return A dose-response model object of class `drc`.
 #' @export
 #'
-get_drm <- function(data, activity_colname="activity", ...){
-  assertthat::assert_that(assertthat::has_name(data, activity_colname),
-                          msg = glue::glue("column {activity_colname} not found in data"))
+get_drm <- function(data, activity_col="activity", ...){
+  assertthat::assert_that(assertthat::has_name(data, activity_col),
+                          msg = glue::glue("column {activity_col} not found"))
   # 4-param logistic model on pre-log-transformed data
-  return(drc::drm(get({{ activity_colname }})~conc_logM,
+  return(drc::drm(get({{ activity_col }})~conc_logM,
                   data = data, fct = drc::L.4()))
 }
 
@@ -57,10 +57,10 @@ get_drm <- function(data, activity_colname="activity", ...){
 #' @export
 #'
 get_EC_logM <- function(data, trt, tgt, response_level=50,
-                   trt_colname="treatment",
-                   tgt_colname="target",
-                   activity_colname="activity"){
-  data <- filter_trt_tgt(data, trt, tgt, trt_colname, tgt_colname)
-  drm <- get_drm(data, activity_colname=activity_colname)
+                   trt_col="treatment",
+                   tgt_col="target",
+                   activity_col="activity"){
+  data <- filter_trt_tgt(data, trt, tgt, trt_col, tgt_col)
+  drm <- get_drm(data, activity_col=activity_col)
 return(drc::ED(drm, response_level, display=FALSE)[1,1])
 }
