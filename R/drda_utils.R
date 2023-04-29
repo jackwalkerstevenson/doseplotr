4e7947f47b5df6fe6888e317eee3373bb88fe9ec
Jack Stevenson <jackstevenson@Jacks-MacBook-Pro.local>
2023-04-28 18:22:00 -0700

    Split EC50 estimator function into two, one to take parameters and one to take an existing model from which to extract params

--- R/drda_utils.R
+++ R/drda_utils.R
@@ -12,7 +12,7 @@ get_hill_slope <- function(model){
 
 #' Get effective logmolar concentration for a response level from a drda model
 #'
-#' @param model A model of type `drda`.
+#' @param model A logistic model of the type returned by [drda::drda()]
 #' @param level The desired response level in percent. Percent of the way from
 #'   the minimum-dose response to the maximum-dose response, regardless of
 #'   whether the response is positive or negative. Default is 50, giving the
@@ -38,18 +38,29 @@ get_EC_nM <- function(model, level=50){
   drda::effective_dose(model, level/100)[1] |> logM_to_nM()
 }
 
-#' Estimate the EC50 of a treatment on a target from a single measurement
+#' Estimate EC50 from one dose-response point and given model parameters
 #'
 #' @param alpha The alpha parameter from a `drda` model.
 #' @param delta The delta parameter from a `drda` model.
 #' @param eta The eta parameter from a `drda` model.
 #' @param x The dose of the single point measurement in log(molar) units
-#' @param y The response of the single point measurement
+#' @param y The activity (NOT percent inhibition) of the single point
+#'   measurement
 #'
 #' @return The estimated EC50 in log10(molar) units
 #' @export
 #'
-EC50_logM_from_point <- function(alpha, delta, eta, x, y){
+EC50_logM_from_point_params <- function(alpha, delta, eta, x, y){
   # 4-parameter logistic equation solved for phi
-  log(delta/(y-alpha)-1)/eta + x
+  unname(log(delta/(y-alpha)-1)/eta + x)
+}
+
+#' Estimate EC50 from one dose-response point and 3 parameters of a given model
+#'
+#' @inheritParams get_hill_slope
+#' @inherit EC50_logM_from_point_params params return
+#' @export
+EC50_logM_from_point_model <- function(model, x, y){
+  c <- stats::coefficients(model)
+  EC50_logM_from_point_params(c["alpha"], c["delta"], c["eta"], x, y)
 }
