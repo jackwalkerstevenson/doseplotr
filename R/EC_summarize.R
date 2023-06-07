89ea8a6afb754d316bf996f3857333bb345e3464
Jack Stevenson <2307047+jackwalkerstevenson@users.noreply.github.com>
2023-06-07 16:38:34 -0400

    Added IC50 to model summary and updated docs

--- R/EC_summarize.R
+++ R/EC_summarize.R
@@ -10,7 +10,9 @@
 #' @return A summary dataframe containing one row for each combination of
 #'   treatment and target in `data` and containing the following columns:
 #'
-#' * EC50_nM
+#' * EC50_nM: the dose at which 50% of the maximum response is achieved, whether
+#'   the response is positive or negative.
+#' * IC50 nM: the dose at which the response is exactly 50.
 #' * EC75_nM: the dose at which 75% of the maximum response is achieved, whether
 #'   the response is positive or negative.
 #' * hill_slope: the `eta` parameter from [drda::drda()]. The rate of growth of
@@ -19,9 +21,11 @@
 summarize_models <- function(data, activity_col="response_norm"){
   data |> dplyr::group_by(.data$treatment, .data$target) |>
   dplyr::summarize(
-    # kind of silly to be fitting the whole model 3x. fine for now
+    # kind of silly to be fitting the whole model multiple times. fine for now
     EC50_nM = data |> filter_trt_tgt(.data$treatment, .data$target) |>
       get_drda(activity_col) |> get_EC_nM(50),
+    IC50_nM = data |> filter_trt_tgt(.data$treatment, .data$target) |>
+      get_drda(activity_col) |> get_IC_nM(50),
     # get_EC_nM() should correctly take 75 to give EC75, unlike cursed ED.drc
     EC75_nM = data |> filter_trt_tgt(.data$treatment, .data$target) |>
       get_drda(activity_col) |> get_EC_nM(75),
