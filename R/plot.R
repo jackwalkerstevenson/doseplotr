781458884da3b2d4bcc737899b02cfd02d29ee7d
Jack Stevenson <2307047+jackwalkerstevenson@users.noreply.github.com>
2023-07-21 14:40:50 -0700

    Edited base_dose_response, added plot_target, updated plot_treatment

--- R/plot.R
+++ R/plot.R
@@ -25,69 +25,33 @@ summarize_response <- function(df, response_col = "response"){
 #' @importFrom ggprism guide_prism_offset_minor
 #' @export
 base_dose_response <- function(plot, x_limits, font_base_size = 14,
-                               xlab = "log10[compound] (M)",
+                               xlab = "log10[treatment] (M)",
                                ylab = "percent of untreated response",
                                legend = TRUE){
   x_min <- x_limits[1]
   x_max <- x_limits[2]
-  minor_x <- log10(rep(1:9, x_max - x_min)*(10^rep(x_min:(x_max - 1), each = 9)))
+  # manually construct minor ticks
+  minor_x <- log10(rep(1:9, x_max - x_min) * (10^rep(x_min:(x_max - 1),
+                                                     each = 9)))
   plot +
-    ggplot2::scale_x_continuous(guide = ggprism::guide_prism_offset_minor(), # end at last tick
+    # ggprism guide to end at last tick
+    ggplot2::scale_x_continuous(guide =ggprism::guide_prism_offset_minor(),
                        breaks = scales::breaks_width(1),
-                       minor_breaks = minor_x) + # manual minor ticks
-    ggplot2::scale_y_continuous(guide = ggprism::guide_prism_offset(),  # end at last tick
+                       minor_breaks = minor_x) +
+    # ggprism guide to end at last tick
+    ggplot2::scale_y_continuous(guide = ggprism::guide_prism_offset(),
                        breaks = c(0,25,50,75,100)) + # y axis ticks 0 to 100
-    ggplot2::coord_cartesian(xlim = x_limits,
-                    ylim = c(0,NA)) + # set y axis zoom locally
-    ggprism::theme_prism(base_size = font_base_size) + # make it look fancy like prism
-    ggplot2::theme(plot.background = ggplot2::element_blank()) + # need for transparent background
-    {if(!legend) ggplot2::theme(legend.position = "none")} +
-    ggplot2::geom_errorbar(ggplot2::aes(ymax = .data$mean_read + .data$sem,
-                               ymin = .data$mean_read - .data$sem,
-                               width = .data$w)) +
+    ggplot2::coord_cartesian(xlim = x_limits, ylim = c(0,NA)) + # y 0 to max
+    ggprism::theme_prism(base_size = font_base_size) + # prism theme
+    ggplot2::theme(plot.background = ggplot2::element_blank()) + # transparent
+    {if(!legend) ggplot2::theme(legend.position = "none")} + # legend option
+    ggplot2::geom_errorbar(ggplot2::aes(ymax = .data$mean_response + .data$sem,
+                                        ymin = .data$mean_response - .data$sem,
+                                        width = .data$w)) +
     # use drm method from drc package to plot dose response curve
     # todo: replace this with same drda method that fits EC50s
     ggplot2::geom_smooth(method = "drm", method.args = list(fct = drc::L.4()), se = FALSE, linewidth = 1) +
     ggplot2::labs(x = xlab, y = ylab)
-}
-
-#' Plot dose-response effects of one treatment on all targets
-#' @param df Dataframe containing data to plot. Must contain columns:
-#'
-#' * treatment
-#' * target
-#' * log_dose
-#' * response column, specified in response_col argument
-#' @param trt Name of treatment to plot
-#' @param response_col Name of column containing response data
-#' @param ... Extra parameters to pass to `base_dose_response()`
-#'
-#' @return A ggplot2 plot of the dose-response effect of the specified treatment
-#'   on all targets
-#' @export
-plot_treatment <- function(df, trt, response_col = "response", ...){
-  # set up color parameters based on number of targets
-  num_targets <- length(unique(df$target))
-  if (num_targets > 8) color_scale <- "turbo" else color_scale <- "viridis"
-  vr <- viridis_range(num_targets)
-  viridis_begin <- vr[1]
-  viridis_end <- vr[2]
-  summary <- df |>
-    filter_trt_tgt(trt = trt) |>
-    dplyr::group_by(.data$target, .data$log_dose) |>
-    summarize_response(response_col = response_col)
-  p <- {ggplot2::ggplot(summary,
-                        ggplot2::aes(x = .data$log_dose,
-                                     y = .data$mean_read, # later take from argument
-                                     color = .data$target)) +
-      ggplot2::geom_point(ggplot2::aes(shape = .data$target), size = 3) +
-      viridis::scale_color_viridis(option = color_scale,
-                          discrete = TRUE,
-                          begin = viridis_begin, end = viridis_end) +
-      ggplot2::labs(title = trt)
-  } |>
-    base_dose_response(...)
-  return(p)
 }
 
 #' Save a plot as an image with predictable plot dimensions
@@ -129,3 +93,97 @@ save_plot <- function(plot, filename,
     height <- nrow * facet_size
   }
   ggplot2::ggsave(filename, plot, bg = "transparent", width = width, height = height)}
+
+#' Plot dose-response effects of one treatment on all targets
+#' @param df Dataframe containing data to plot. Must contain columns:
+#'
+#' * treatment
+#' * target
+#' * log_dose
+#' * response column, specified in response_col argument
+#' @param trt Name of treatment to plot
+#' @param response_col Name of column containing response data
+#' @param ... Extra parameters to pass to `base_dose_response()`
+#'
+#' @return A ggplot2 plot of the dose-response effect of the specified treatment
+#'   on all targets
+#' @export
+plot_treatment <- function(df, trt,
+                           response_col = "response",
+                           x_limits = NULL,
+                           ...){
+  summary <- df |>
+    filter_trt_tgt(trt = trt) |>
+    dplyr::group_by(.data$target, .data$log_dose) |>
+    summarize_response(response_col = response_col)
+  # set up color parameters based on number of targets
+  num_targets <- length(unique(summary$target))
+  vr <- viridis_range(num_targets)
+  vr_begin <- vr[[1]]
+  vr_end <- vr[[2]]
+  vr_option <- vr[[3]]
+  # calculate x limits if not specified
+  if(is.null(x_limits)){
+    x_min <- floor(min(summary$log_dose))
+    x_max <- ceiling(max(summary$log_dose))
+    x_limits <- c(x_min, x_max)
+  }
+  p <- {ggplot2::ggplot(summary,
+                        ggplot2::aes(x = .data$log_dose,
+                                     y = .data$mean_response,
+                                     color = .data$target)) +
+      ggplot2::geom_point(ggplot2::aes(shape = .data$target), size = 3) +
+      viridis::scale_color_viridis(discrete = TRUE, option = vr_option,
+                                   begin = vr_begin, end = vr_end) +
+      ggplot2::labs(title = trt)
+  } |>
+    base_dose_response(x_limits = x_limits, ...)
+  return(p)
+}
+
+#' Plot dose-response effects of one target on all treatments
+#' @param df Dataframe containing data to plot. Must contain columns:
+#'
+#' * treatment
+#' * target
+#' * log_dose
+#' * response column, specified in response_col argument
+#' @param tgt Name of target to plot
+#' @param response_col Name of column containing response data
+#' @param ... Extra parameters to pass to `base_dose_response()`
+#'
+#' @return A ggplot2 plot of the dose-response effect of the specified target on
+#'   all treatments
+#' @export
+plot_target <- function(df, tgt,
+                           response_col = "response",
+                           x_limits = NULL,
+                           ...){
+  summary <- df |>
+    filter_trt_tgt(tgt = tgt) |>
+    dplyr::group_by(.data$treatment, .data$log_dose) |>
+    summarize_response(response_col = response_col)
+  # set up color parameters based on number of treatments
+  num_treatments <- length(unique(summary$treatment))
+  vr <- viridis_range(num_treatments)
+  vr_begin <- vr[[1]]
+  vr_end <- vr[[2]]
+  vr_option <- vr[[3]]
+  # calculate x limits if not specified
+  if(is.null(x_limits)){
+    x_min <- floor(min(summary$log_dose))
+    x_max <- ceiling(max(summary$log_dose))
+    x_limits <- c(x_min, x_max)
+  }
+  p <- {ggplot2::ggplot(summary,
+                        ggplot2::aes(x = .data$log_dose,
+                                     y = .data$mean_response,
+                                     color = .data$treatment)) +
+      ggplot2::geom_point(ggplot2::aes(shape = .data$treatment), size = 3) +
+      viridis::scale_color_viridis(discrete = TRUE, option = vr_option,
+                                   begin = vr_begin, end = vr_end) +
+      ggplot2::labs(title = tgt)
+  } |>
+    base_dose_response(x_limits = x_limits, ...)
+  return(p)
+}
