12a22465a98d3f34334507d92bd9c58bfa8a3368
Jack Stevenson <2307047+jackwalkerstevenson@users.noreply.github.com>
2023-08-03 16:05:01 -0700

    Added functions to generate minor breaks for plots

--- R/aesthetic_utils.R
+++ R/aesthetic_utils.R
@@ -33,3 +33,56 @@ viridis_range <- function(n, reverse = TRUE){
   else{
     return(c(low, high, option))}
 }
+
+#' Generate minor breaks for an axis in non-log-transformed units
+#'
+#' This function generates logistic minor axis breaks to use on an axis that is
+#' in original, non-log-transformed units, such that the data being plotted is
+#' e.g. "1000" rather than "3".
+#' @param min_val The minimum value to plot, or the log floor of same (0.1 and
+#'   0.3 will both be converted to 0.1).
+#' @param max_val The maximum value to plot, or the log ceiling of same (500 and
+#'   1000 will both be converted to 1000).
+#' @param log_units Whether the units of the minimum and maximum values are in
+#'   log-transformed units. Default is FALSE.
+#' @return A vector of minor axis breaks in raw units (not log-transformed).
+#' @export
+#' @examples
+#' minor_breaks(1, 1000)
+#' minor_breaks(3, 857)
+#' minor_breaks(-12, -4, log = TRUE)
+#' minor_breaks(-12.5, -3.8, log = TRUE)
+minor_breaks <- function(min_val, max_val, log_units = FALSE){
+  # convert min_val and max_val into log units if not already-------------------
+  if(log_units){
+    min_val <- floor(min_val)
+    max_val <- ceiling(max_val)
+  } else{
+    assertthat::assert_that(min_val > 0 & max_val > 0,
+                            msg =
+                              glue::glue("Limits must be greater than zero ",
+                                         "to generate log-scaled axis breaks. ",
+                                         "Did you mean to specify ",
+                                         "`log_units = TRUE`?"))
+    min_val <- floor(log10(min_val))
+    max_val <- ceiling(log10(max_val))
+  }
+  rep(1:9, max_val - min_val) * # repeat 1-9 for each log interval
+    (10^rep(min_val:(max_val - 1), each = 9)) # times appropriate powers of 10
+}
+
+#' Generate minor breaks for an axis in log units
+#'
+#' This function generates logistic minor axis breaks for an axis that uses
+#' log-transformed units, such that the data being plotted is e.g. -3 rather
+#' than 0.001.
+#' @param min_val The minimum value to plot, or log floor of same, in log-transformed units (-8.3 and -8 will both be converted to -8).
+#' @param max_val The maximum value to plot, or log ceiling of same, in log-transformed units (-4.5 and -4 will both be converted to -4).
+#' @return A vector of minor axis breaks in log-transformed units.
+#' @export
+#' @examples
+#' minor_breaks_log(-12, -4)
+#' minor_breaks_log(-12.2, -3.6)
+minor_breaks_log <- function(min_val, max_val){
+  log10(minor_breaks(min_val, max_val, log_units = TRUE))
+}
